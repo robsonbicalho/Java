dc29f3baaf35d270d5e2900f60a05bbcff5f86dc
robsonbicalho <robsbicalho@gmail.com>
2023-04-08 12:52:16 -0300

    quarta aula

---
+++ Quarta_Aula/src/Algoritmo01.java
@@ -0,0 +1,48 @@
+import javax.swing.JOptionPane;
+
+public class Algoritmo01 {
+    public static void main(String[] args) {
+        String entrada;
+        int nota, salario, idade;
+        String  sexo;
+
+        do {
+            entrada = JOptionPane.showInputDialog("Informe uma nota de 0 a 10: ");
+            nota = Integer.parseInt(entrada);
+            while (nota < 0 || nota > 10) {
+            JOptionPane.showMessageDialog(null, "Dados incorretos");
+            entrada = JOptionPane.showInputDialog("Informe uma nota de 0 a 10: ");
+            nota = Integer.parseInt(entrada);
+        	}
+            
+            entrada = JOptionPane.showInputDialog("Informe o salário: ");
+            salario = Integer.parseInt(entrada);
+            while (salario < 0) {
+                JOptionPane.showMessageDialog(null, "Dados incorretos");
+                entrada = JOptionPane.showInputDialog("Informe o salário: ");
+                salario = Integer.parseInt(entrada);
+            	}
+
+            
+            sexo = JOptionPane.showInputDialog("Informe o sexo (m ou f): ");
+           
+            while (!sexo.equals("f") && !sexo.equals("m")) { 
+                JOptionPane.showMessageDialog(null, "Dados incorretos");
+                sexo = JOptionPane.showInputDialog("Informe o sexo (m ou f): ");
+            	}
+            
+            entrada = JOptionPane.showInputDialog("Informe a idade: ");
+            idade = Integer.parseInt(entrada);
+            while (idade < 0) {
+                JOptionPane.showMessageDialog(null, "Dados incorretos");
+                entrada = JOptionPane.showInputDialog("Informe a idade: ");
+                idade = Integer.parseInt(entrada);
+            	}
+            
+            entrada = JOptionPane.showInputDialog("Os dados informados estão corretos.\nDeseja continuar? (S)im ou (N)ão");
+        } while (entrada.equalsIgnoreCase("s") || entrada.equalsIgnoreCase("sim"));
+
+        System.exit(0);
+    }
+}
+
---
+++ Quarta_Aula/src/Algoritmo02.java
@@ -0,0 +1,54 @@
+import javax.swing.JOptionPane;
+
+public class Algoritmo02 {
+    public static void main (String args[]) {
+    	 String entrada;
+         int nota, salario, idade;
+         String  sexo;
+
+         do {
+             entrada = JOptionPane.showInputDialog("Informe uma nota de 0 a 10: ");
+             nota = Integer.parseInt(entrada);
+             while (nota < 0 || nota > 10) {
+             JOptionPane.showMessageDialog(null, "Dados incorretos");
+             entrada = JOptionPane.showInputDialog("Informe uma nota de 0 a 10: ");
+             nota = Integer.parseInt(entrada);
+         	}
+             
+             entrada = JOptionPane.showInputDialog("Informe o salário: ");
+             salario = Integer.parseInt(entrada);
+             while (salario < 0) {
+                 JOptionPane.showMessageDialog(null, "Dados incorretos");
+                 entrada = JOptionPane.showInputDialog("Informe o salário: ");
+                 salario = Integer.parseInt(entrada);
+             	}
+
+             
+             sexo = JOptionPane.showInputDialog("Informe o sexo (m ou f): ");
+            
+             while (!sexo.equals("f") && !sexo.equals("m")) { 
+                 JOptionPane.showMessageDialog(null, "Dados incorretos");
+                 sexo = JOptionPane.showInputDialog("Informe o sexo (m ou f): ");
+             	}
+             
+             entrada = JOptionPane.showInputDialog("Informe a idade: ");
+             idade = Integer.parseInt(entrada);
+             while (idade < 0) {
+                 JOptionPane.showMessageDialog(null, "Dados incorretos");
+                 entrada = JOptionPane.showInputDialog("Informe a idade: ");
+                 idade = Integer.parseInt(entrada);
+             	}
+             
+            int resp = JOptionPane.showConfirmDialog(null, "Os dados informados estão corretos.\n\nDeseja continuar?");
+            if (resp == JOptionPane.YES_OPTION)
+                entrada = "sim";
+            else if (resp == JOptionPane.NO_OPTION)
+                entrada = "não";
+        } while (entrada.equalsIgnoreCase("s") || entrada.equalsIgnoreCase("sim"));
+        System.exit(0);
+    }
+}
+
+
+
+
